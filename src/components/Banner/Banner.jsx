c1e78df97bb76f7869fa37906a32fe5eadcd9958
Yashvardhan Jaiswal <yashvardhanjaiswal7105@gmail.com>
2024-10-18 06:37:50 +0530

    first commit

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Carousel } from "react-bootstrap";
+import sliderImg from "../../assets/images/slider/1.png";
+import sliderImg1 from "../../assets/images/slider/2.png";
+import "../Banner/banner.css"
+
+const Banner = () => {
+  return (
+    <>
+      <section className="slider">
+        <Carousel variant="dark">
+          <Carousel.Item>
+            <img src={sliderImg} className="d-block w-100" alt="First slide" />
+            <Carousel.Caption>
+              <div className="slider_des">
+                <h5 className="heading">
+                  JOURNEY TO <span>EXPLORE THE WORLD</span>
+                </h5>
+                <p className="sub_text">
+                We live in a wonderful world that is full of beauty, charm and adventure. There is no end to the adventures we can have if only we seek them with our eyes open.
+                </p>
+              </div>
+            </Carousel.Caption>
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <img src={sliderImg1} className="d-block w-100" alt="First slide" />
+            <Carousel.Caption>
+              <div className="slider_des">
+                <h5 className="heading">
+                  BEAUTIFUL PLACE <span>TO VISIT</span>
+                </h5>
+                <p className="sub_text">
+                    Perhaps travel cannot prevent bigotry, but by demonstrating that all peoples cry, laugh, eat, worry, and die, it can introduce the idea that if we try and understand each other, we may even become friends.
+                </p>
+              </div>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </section>
+    </>
+  );
+};
+
+export default Banner;
